e874afb84799a5cc4bc1ecb2dee643d6aaa07f20
Alexis Wurth <alexis.wurth57@gmail.com>
2017-02-22 18:40:17 +0100

    Added directives for displaying form errors

---
+++ src/app/common/field-error.directive.js
@@ -0,0 +1,13 @@
+
+export default function FieldErrorDirective () {
+  return {
+    restrict: 'A',
+    link: (scope, element, attrs) => {
+      scope.$watch(attrs.fieldError, (value) => {
+        if (value) {
+          element.addClass('error')
+        }
+      })
+    }
+  }
+}
